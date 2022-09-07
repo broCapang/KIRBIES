6f9ec85d8743abe34b8d54a3954100dd8932788e
CAPANG JABBA <ihaziq18.ih@gmail.com>
2022-09-08 01:47:30 +0800

    yes

--- Exercises/kickstart/test.py
+++ Exercises/kickstart/test.py
@@ -1,2 +1,15 @@
-C=25
-print(len(15,25+1))
+N=5
+M=33
+
+
+a=M//N
+b=M%N
+for i in range(b):
+    for i in range(a+1):
+        print("*",end="") 
+    print()
+for i in range(N-b):
+    for i in range(a):
+        print("*",end="") 
+    print()
+    
